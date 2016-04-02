5bdca6cc43abf1f3d7473cd30d2c6e7060f38bc5
satoshi-n <s.nanya@catarizm.co.jp>
2016-04-02 15:05:44 +0900

    Initial commit

---
+++ app/src/main/java/nanya/s/viewdraghelpertest/DragLayout.java
@@ -0,0 +1,67 @@
+package nanya.s.viewdraghelpertest;
+
+import android.content.Context;
+import android.support.v4.view.MotionEventCompat;
+import android.support.v4.widget.ViewDragHelper;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by s.nanya on 2016/04/02.
+ */
+public class DragLayout extends LinearLayout {
+
+    private final ViewDragHelper mDragHelper;
+    private View mDragView;
+
+    public DragLayout(Context context) {
+        this(context, null);
+    }
+
+    public DragLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public DragLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mDragHelper = ViewDragHelper.create(this, 1.0f, new DragHelperCallback());
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        final int action = MotionEventCompat.getActionMasked(ev);
+        if (action == MotionEvent.ACTION_CANCEL || action == MotionEvent.ACTION_UP) {
+            mDragHelper.cancel();
+            return false;
+        }
+        return mDragHelper.shouldInterceptTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        mDragHelper.processTouchEvent(ev);
+        return true;
+    }
+
+    private class DragHelperCallback extends ViewDragHelper.Callback {
+
+        @Override
+        public boolean tryCaptureView(View child, int pointerId) {
+            return true;
+        }
+
+        @Override
+        public int clampViewPositionVertical(View child, int top, int dy) {
+            return top;
+        }
+
+        @Override
+        public int clampViewPositionHorizontal(View child, int left, int dx) {
+            return left;
+        }
+
+    }
+
+}
